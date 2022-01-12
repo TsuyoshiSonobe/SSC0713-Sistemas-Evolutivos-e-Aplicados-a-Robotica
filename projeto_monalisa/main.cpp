771d885afdf7cf3975a91e67939619bdbdf921a5
TsuyoshiSonobe <tsuyoshi.sonobe@usp.br>
2022-01-11 23:29:21 -0300

    Comentarios adicionados

--- projeto_monalisa/main.cpp
+++ projeto_monalisa/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
@@ -12,46 +13,41 @@ using namespace cv;
 
 int main(){
 
-    Mat img = imread("monalisa.jpg", IMREAD_COLOR);
-    resize(img, img, Size(100, 100), INTER_LINEAR);   // Redefinindo tamanho da imagem
+    Mat img = imread("monalisa.jpg", IMREAD_COLOR);     // Leitura da imagem
+    resize(img, img, Size(100, 100), INTER_LINEAR);     // Redefinindo tamanho da imagem
 
+    // Alterando imagem de RGB (colorida) para Gray Scale (escala de cinza)
     Mat imgGrayScale;
     cvtColor(img, imgGrayScale, COLOR_BGR2GRAY);
 
-    Mat black;
-    inRange(imgGrayScale, 127, 255, black);  
-
-    // Visualizar imagem original
-    /*namedWindow("Original", WINDOW_NORMAL);
-    namedWindow("Gray Scale", WINDOW_NORMAL);
-    namedWindow("Black", WINDOW_NORMAL);
+    // Mostrando imagem original
+    namedWindow("Original", WINDOW_NORMAL);
     imshow("Original", img);
-    imshow("Gray Scale", imgGrayScale);
-    imshow("Black", black);
-    waitKey(0);
-    destroyAllWindows();*/
 
-    // Transformando Mat em array
+    // Linearizando a imagem, ou seja, transformamos de matriz para array,
+    // colocando os valores dos pixels da imagem em um array que sera usado como referencia para comparacao e calculo do fitness
     vector<uchar> array;
-    if (black.isContinuous()) {
+    if (imgGrayScale.isContinuous()) {
         array.assign(imgGrayScale.data, imgGrayScale.data + imgGrayScale.total()*imgGrayScale.channels());
     }
     else {
-        for (int i = 0; i < black.rows; ++i) {
+        for (int i = 0; i < imgGrayScale.rows; ++i) {
             array.insert(array.end(), imgGrayScale.ptr<uchar>(i), imgGrayScale.ptr<uchar>(i)+imgGrayScale.cols*imgGrayScale.channels());
         }
     }
-
     int *referencia = new int[array.size()];
     copy(array.begin(), array.end(), referencia);
 
-    /*for(int i = 0; i < array.size(); i++){
-        if(referencia[i] == 255){
-            referencia[i] = 1;
-        }
-    } */
+    // Abrindo arquivo para escrita dos dados do algoritmo evolutivo
+    FILE *file1;
+    file1 = fopen("dados.txt", "w");
+    if(file1 == NULL){
+        printf("Erro na abertura do arquivo");
+        return 1;
+    }
 
-    // Algoritmo Genetico
+    
+    // Algoritmo Evolutivo
     const int nGene = array.size();                     // numero de genes por individuo
     const int nAvaliacoes = 2000;                       // numero de avaliacoes realizadas
     const int nGeneAvaliados = nGene/nAvaliacoes;       // numero de genes avaliados por vez
@@ -62,19 +58,26 @@ int main(){
     int *popAtual = new int[nInd * nGeneAvaliados];     // matriz de individuos com parte dos genes (os que estao sendo avaliados)
     int tamImgFinal = sqrt(array.size());               // tamanho das dimensoes da imagem final (imagem quadrada)
     uint8_t *imgFinal = new uint8_t[tamImgFinal * tamImgFinal]; // matriz que armazenara a imagem final
-    Mat finalImg;
+    Mat finalImg;                                       // objeto que armazenara a imagem final para ser mostrada
 
     // Criacao da populacao inicial
     srand(time(NULL));    
+    // gera uma matriz na qual o numero de linhas eh o numero de individuos da geracao
+    // e o numero de colunas eh o numero de genes (quantidade de pixels da imagem utilizada),
+    // depois inicia a matriz com valores aleatorios de 0 a 255
     for(int i = 0; i < nInd; i++){          
         for(int j = 0; j < nGene; j++){
-            pop[i * nGene + j] = rand() % 256;
+            pop[i * nGene + j] = rand() % 256;  
         }
     }
     
+    // Separamos a analise da imagem em partes, utilizando o algoritmo evolutivo para avaliar uma quantidade de pixels 
+    // definida por nGeneAvaliados de cada vez, e repetindo o processo por nAvaliacoes vezes ate que toda a imagem seja avaliada
     int aux = 0, atual = 0;
     for(int n = 0; n < nAvaliacoes; n++){
         
+        // Coloca os genes (pixels) que serao avaliados em popAtual
+        // (dessa forma conseguimos fazer alteracoes sem modificar os outros genes da populacao)
         for(int i = 0; i < nInd; i++){
             aux = nGeneAvaliados * n;          
             for(int j = 0; j < nGeneAvaliados; j++){
@@ -86,29 +89,23 @@ int main(){
         // Passando pelas geracoes
         for(int g = 0; g < nGer; g++){    
 
-            // Imprimindo a populacao da geracao
-            //cout << "Geração " << g << endl;
-            /*for(int i = 0; i < nInd; i++){
-                cout << "Individuo " << i << endl;
-                for(int j = 0; j < nGene; j++){
-                    cout << pop[i * nGene + j] << " ";
-                }
-                cout << endl;
-            }*/
-
             // Avaliacao e selecao da populacao da geracao
-            for(int i = 0; i < nInd; i++){           // passando por cada individuo
+
+            // Passando por cada individuo
+            for(int i = 0; i < nInd; i++){           
                 aux = nGeneAvaliados * n;
-                for(int j = 0; j < nGeneAvaliados; j++){      // passando por cada gene do individuo
+                // Passando por cada gene do individuo
+                for(int j = 0; j < nGeneAvaliados; j++){      
                     fitInd += -1*abs(popAtual[i * nGeneAvaliados + j]-referencia[aux]);
                     aux++;
                     fitGer += fitInd;
                 }
-            //    cout << "Fitness do individuo " << i << ": " << fitInd << endl;
+                // Melhor individuo
                 if(fitInd > fitMInd){
                     fitMInd = fitInd;
                     mInd = i;
                 }
+                // Pior individuo
                 if(fitInd < fitPInd){
                     fitPInd = fitInd;
                     pInd = i;
@@ -116,7 +113,8 @@ int main(){
                 fitInd = 0;
             }
             
-            // Croosover
+            // Crossover
+            // (copiamos uma parte de tamanho aleatorio do melhor individuo para os demais)
             int corte = rand() % nGeneAvaliados;
             for(int i = 0; i < nInd; i++){
                 for(int j = 0; j < corte; j++){
@@ -125,25 +123,26 @@ int main(){
             }
             
             // Mutacao
+            // (muta um certo numero de genes de todos os individuos, exceto do melhor, somando ou subtraindo um certo valor)
             int pontoMutacao, operacao;
-            //if (rand()%11 < 7) {                     // probabilidade de mutacao
-                for(int i = 0; i < nInd; i++){
-                    if(i != mInd){                                  // nao muta o melhor individuo
-                        for(int k = 0; k < taxMut; k++){
-                            pontoMutacao = rand() % nGeneAvaliados;            // muta um gene aleatorio
-                            operacao = rand() % 2;
-                            if(operacao == 0){
-                                if(popAtual[i * nGeneAvaliados + pontoMutacao] + valMut < 256)
-                                    popAtual[i * nGeneAvaliados + pontoMutacao] += valMut;
-                            }
-                            else{
-                                if(popAtual[i * nGeneAvaliados + pontoMutacao] - valMut > 0)
-                                    popAtual[i * nGeneAvaliados + pontoMutacao] -= valMut;
-                            }
+            for(int i = 0; i < nInd; i++){
+                if(i != mInd){                                  // nao muta o melhor individuo
+                    for(int k = 0; k < taxMut; k++){            // muta o numero de genes definido por taxMut 
+                        pontoMutacao = rand() % nGeneAvaliados; // muta um gene aleatorio
+                        operacao = rand() % 2;
+                        if(operacao == 0){
+                            // Soma valMut ao gene, caso o valor da soma nao ultrapasse 255
+                            if(popAtual[i * nGeneAvaliados + pontoMutacao] + valMut < 256)
+                                popAtual[i * nGeneAvaliados + pontoMutacao] += valMut;  
+                        }
+                        else{
+                            // Subtrai valMut do gene, caso o valor da subtracao seja maior que zero
+                            if(popAtual[i * nGeneAvaliados + pontoMutacao] - valMut > 0)
+                                popAtual[i * nGeneAvaliados + pontoMutacao] -= valMut;  
                         }
                     }
                 }
-            //}
+            }
             
             // Trocando o pior individuo pelo melhor
             for(int j = 0; j < nGeneAvaliados; j++){
@@ -151,13 +150,12 @@ int main(){
             }
 
             // Resultados da geracao
-            /*cout << "Numero do melhor individuo da geracao: " << mInd << " -> Fitness: " << fitMInd << endl;
             fitMedioGer = (fitGer/nInd);       
-            cout << "Fitness medio da geração: " << fitMedioGer << endl;
             fitGer = 0; 
-            cout << endl;*/
+            // Gravando a geração, melhor indivíduo, nota dele e a nota média da geração
+            fprintf(file1,"%d,%d,%d,%d\n", g ,mInd, fitMInd, fitMedioGer);
             
-            // Coloca os individuos da populacao atual na matriz populacao
+            // Coloca os individuos da populacao atual na matriz que contem toda a populacao com todos os genes
             for(int i = 0; i < nInd; i++){ 
                 aux = nGeneAvaliados*n;         
                 for(int j = 0; j < nGeneAvaliados; j++){
@@ -166,14 +164,13 @@ int main(){
                 }
             }
 
-            // Transforma o array binario em matriz de cores (branco e preto, por enquanto)
+            // Utiliza o melhor individuo da geracao para gerar a imagem final,
+            // mudando do vetor com os valores dos pixels para a matriz que gera a imagem
             int k = 0;
             for(int i = 0; i < tamImgFinal; i++){
                 for(int j = 0; j < tamImgFinal; j++){
                     imgFinal[i * tamImgFinal + j] = pop[mInd * nGene + k];
                     k++;
-                    if(imgFinal[i * tamImgFinal + j] == 1)
-                        imgFinal[i * tamImgFinal + j] = 255;
                 }
             }
 
@@ -184,13 +181,16 @@ int main(){
             imshow("Imagem Final", finalImg);
             waitKey(1);
             
-            // Encerra antes caso o melhor individuo seja o ideal
+            // Encerra antes caso o melhor individuo esteja proximo do ideal
+            // (adicionamos um certo limite para que os pixels nao precisem todos terem exatamente os mesmos valores dos originais,
+            // mas que sejam proximos o suficiente para que a imagem reconstruida seja muito parecida com a original)
             if(fitMInd > (nGeneAvaliados * (-1) - 11)){
                 break;
             }
         }
 
         // Copia os genes avaliados do melhor individuo para todos os outros
+        // (isso faz com que os pixels ja avaliados e escolhidos sejam iguais para todos da populacao)
         for(int i = 0; i < nInd; i++){
             for(int j = 0; j < nGeneAvaliados; j++){
                 popAtual[i * nGeneAvaliados + j] = popAtual[mInd * nGeneAvaliados + j];
@@ -205,18 +205,23 @@ int main(){
         }
 
         // Reseta os valores de melhor e pior individuos
-        fitMInd = -1000000; 
-        mInd = -1;                     // fitness e numero do melhor individuo
-        fitPInd = nGene;
-        pInd = -1;                     // fitness e numero do pior individuo
+        fitMInd = -1000000;             
+        mInd = -1;                     
+        fitPInd = nGene;                
+        pInd = -1;                     
 
     }
 
+    // Espera que o usuario pressione uma tecla para fechar a janela da imagem final
     waitKey(0);
-    destroyWindow("finalImg");
+    destroyAllWindows();
 
+    // Desalocando a memoria utilizada
     delete [] imgFinal;
     delete [] referencia;
+    
+    // Fechando arquivo dados.txt
+    fclose(file1);
 
     return 0;
 }
